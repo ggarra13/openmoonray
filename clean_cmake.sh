e1cb9e05d477dc3351e99865ad15b6d645e0184c
Gonzalo Garramuño <ggarra13@gmail.com>
2023-12-20 20:01:30 -0300

    Updated clean_cmake.sh script.

--- clean_cmake.sh
+++ clean_cmake.sh
@@ -3,3 +3,5 @@
 find moonray -name 'CMakeFiles' -exec rm -rf {} \; 
 find moonray -name 'cmake_install.cmake' -exec rm -rf {} \; 
 find moonray -name 'Makefile2' -exec rm -rf {} \;
+find moonray -name 'Makefile' -exec rm -rf {} \;
+find moonray -name 'build.make' -exec rm -rf {} \;
